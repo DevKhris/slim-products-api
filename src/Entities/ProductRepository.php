e1dbe9e71eeafb695822526943e7f57ceb2eef55
Khris <contact@devkhris.com>
2022-10-20 16:34:00 -0500

    fixed flush on repository

--- src/Entities/ProductRepository.php
+++ src/Entities/ProductRepository.php
@@ -75,7 +75,7 @@ class ProductRepository extends EntityRepository
         $product->setKeywords($data['keywords']);
 
         $this->_em->persist($product);
-        $this->em->flush();
+        $this->_em->flush();
     }
 
     public function update(Int $id, array $data)
